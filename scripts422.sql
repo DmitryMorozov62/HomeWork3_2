45f930bc73bfb616f7e4d927c28cd78fd5780ea7
DmitryMorozov62 <62morozov.dima.90@gmail.com>
2023-07-10 23:44:43 +0300

    4.2. Управление схемами баз данных

---
+++ scripts422.sql
@@ -0,0 +1,25 @@
+CREATE TABLE car
+(
+    id    BIGSERIAL PRIMARY KEY,
+    brand VARCHAR(15)             NOT NULL,
+    model VARCHAR(31)             NOT NULL,
+    price INT CHECK ( price > 0 ) NOT NULL
+);
+
+CREATE TABLE human
+(
+    id             BIGSERIAL PRIMARY KEY,
+    name           VARCHAR(15)                NOT NULL,
+    age            INT CHECK ( age > 16 )     NOT NULL,
+    driver_license BOOLEAN DEFAULT true       NOT NULL,
+    car_id         BIGINT REFERENCES car (id) NOT NULL
+);
+
+INSERT INTO car(brand, model, price)
+VALUES ('BMW', 'X6', 10000000),
+       ('Audi', 'Q6', 8000000);
+
+INSERT INTO human(name, age, car_id)
+VALUES ('Ivan', 20, 2),
+       ('Petr', 30, 1);
+
